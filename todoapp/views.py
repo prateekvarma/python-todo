369d765b8691d34f09ca4cf666e0eb667f4b25c8
unknown <prtk.varma@gmail.com>
2020-09-17 20:58:46 -0600

    signup form added

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def signupuser(request):
+    return render(request, 'todoapp/signupuser.html', {'form':UserCreationForm()})
