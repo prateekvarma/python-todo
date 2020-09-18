362e9761b2c60ea8580241716516bd0c735e7fb9
unknown <prtk.varma@gmail.com>
2020-09-18 00:42:41 -0600

    fixed typo in user creation

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 def signupuser(request):
     if request.method == 'GET' :
@@ -8,9 +9,12 @@ def signupuser(request):
     else:
         #check password
         if request.POST['password1'] == request.POST['password2']:
-            #create a new user
-            user = User.object.create_user(request.POST['username'], password=request.POST['password1'])
-            user.save()
+            try:               
+                #create a new user
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+            except IntegrityError:
+                return render(request, 'todoapp/signupuser.html', {'form':UserCreationForm(), 'error':'Username alredy exists'})
         else:
             #passwords dont match
             return render(request, 'todoapp/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords dont match'})
