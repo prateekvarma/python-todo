d43e529dc116bb45c464d177e9419c7e81637229
unknown <prtk.varma@gmail.com>
2020-09-18 00:34:02 -0600

    signup password validation added

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 def signupuser(request):
-    return render(request, 'todoapp/signupuser.html', {'form':UserCreationForm()})
+    if request.method == 'GET' :
+        return render(request, 'todoapp/signupuser.html', {'form':UserCreationForm()})
+    else:
+        #check password
+        if request.POST['password1'] == request.POST['password2']:
+            #create a new user
+            user = User.object.create_user(request.POST['username'], password=request.POST['password1'])
+            user.save()
+        else:
+            #passwords dont match
+            return render(request, 'todoapp/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords dont match'})
+            
