f113780a368c61dddf521bc9702d5ea48aa04551
unknown <prtk.varma@gmail.com>
2020-09-24 14:58:22 -0600

    Fixed Todo model where datetime can be blank on admin

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#Difference in blank & null -> Blank is used in textfield and chars, while null is used for datetime
+#Difference in blank & null -> Blank is used in textfield and chars, while null+blank is used for datetime
 
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    datecomlpleted = models.DateTimeField(null=True)
+    datecomlpleted = models.DateTimeField(null=True, blank=True)
     important = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
