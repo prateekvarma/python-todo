191aeec696b2c28478a0a77bcec5a2d3057220a5
unknown <prtk.varma@gmail.com>
2020-09-24 14:49:45 -0600

    Todo model created and migrated

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Difference in blank & null -> Blank is used in textfield and chars, while null is used for datetime
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecomlpleted = models.DateTimeField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
