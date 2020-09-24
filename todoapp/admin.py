b3dce3b7ac04594a5587f8b5a6cc29dd918ea658
unknown <prtk.varma@gmail.com>
2020-09-24 15:06:03 -0600

    configured admin to display date created field, since by default it is not displayed by admin

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Todo
 
+#Display the date created field on admin, since it does not display by default
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
 # Register your models here.
-admin.site.register(Todo)
+admin.site.register(Todo, TodoAdmin)
