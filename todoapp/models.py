bd1164b4ea4663a16358cdb62d33dbdadc6b1820
unknown <prtk.varma@gmail.com>
2020-09-24 15:00:35 -0600

    configured Todo model to display title name in admin

--- todoapp/models.py
+++ todoapp/models.py
@@ -10,3 +10,6 @@ class Todo(models.Model):
     datecomlpleted = models.DateTimeField(null=True, blank=True)
     important = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
