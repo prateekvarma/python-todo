b8e804eaa596b8ae3722304f81af1330073bbb73
unknown <prtk.varma@gmail.com>
2020-10-07 02:20:02 -0600

    Delete todo function added

--- todoapp/views.py
+++ todoapp/views.py
@@ -88,3 +88,9 @@ def completetodo(request, todo_pk):
         todo.datecomlpleted = timezone.now()
         todo.save()
         return redirect('currenttodos')
+
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
